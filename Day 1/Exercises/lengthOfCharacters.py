9de46b7de7ed027f576a5c41f2ae7e6e5a0b0fed
redkad <tengecollins@yahoo.com>
2021-01-03 07:43:36 +0000

    initial commit

---
+++ Day 1/Exercises/lengthOfCharacters.py
@@ -0,0 +1,2 @@
+name = input('What is your name?: ')
+print(f'The length of characters in your name {name} is: {len(name)}')
---
+++ Day 1/Exercises/swappingVariableValues.py
@@ -0,0 +1,9 @@
+a = input('a: ')
+b = input('b: ')
+
+a_ = a
+a = b
+b = a_
+
+print(f'a = {a}')
+print(f'b = {b}')
