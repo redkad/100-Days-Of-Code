5ba5466a31b73658c424bb45f99eb00b72140889
redkad <tengecollins@yahoo.com>
2021-02-06 03:12:09 +0000

    Added features to main.py

--- Day 20/snake_game/main.py
+++ Day 20/snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -11,13 +12,13 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 is_game_on = True
 
@@ -26,5 +27,21 @@ while is_game_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food.
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        score.game_over()
+
+    for snake_body in snake.snake_body[1:]:
+        if snake.head.distance(snake_body) < 10:
+            is_game_on = False
+            score.game_over()
 
 screen.exitonclick()
