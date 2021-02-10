d563c98de291211a3e1266291565e3e8b9de4bad
redkad <tengecollins@yahoo.com>
2021-02-10 17:50:18 +0000

    Fixes

--- Day 22/pong-game/main.py
+++ Day 22/pong-game/main.py
@@ -17,10 +17,10 @@ left_paddle = Paddle((-350, 0))
 ball = Ball()
 scoreboard = ScoreBoard()
 
-screen.onkey(right_paddle.go_up, "Up")
-screen.onkey(right_paddle.go_down, "Down")
-screen.onkey(left_paddle.go_up, "w")
-screen.onkey(left_paddle.go_down, "s")
+screen.onkeypress(right_paddle.go_up, "Up")
+screen.onkeypress(right_paddle.go_down, "Down")
+screen.onkeypress(left_paddle.go_up, "w")
+screen.onkeypress(left_paddle.go_down, "s")
 
 screen.listen()
 
