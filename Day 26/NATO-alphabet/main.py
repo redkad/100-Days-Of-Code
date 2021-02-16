dc0bbd7ee332ba6bd47c3f336632f668fecbe7eb
redkad <tengecollins@yahoo.com>
2021-02-16 06:27:29 +0000

    Generating nato alphabets

--- Day 26/NATO-alphabet/main.py
+++ Day 26/NATO-alphabet/main.py
@@ -20,8 +20,18 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(type(data))
+new_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+# print(new_dict)
 
+
+word = input("Enter a word: ").upper()
+l = []
+new_list = [new_dict[letter] for letter in word]
+print(new_list)
+
+# new_dict = {new_key:new_value for (key, value) in new_dict.items()}
+# for letter in word:
+#     print(new_dict[letter])
