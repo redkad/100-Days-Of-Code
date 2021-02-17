246ef35b140c55ceb84b0398987da3cac31b4b2a
redkad <tengecollins@yahoo.com>
2021-02-17 08:54:40 +0000

    Generating nato alphabets

--- Day 26/NATO-alphabet/main.py
+++ Day 26/NATO-alphabet/main.py
@@ -28,7 +28,7 @@ new_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 
 word = input("Enter a word: ").upper()
-l = []
+
 new_list = [new_dict[letter] for letter in word]
 print(new_list)
 
